c33048de5355c3240f9247f693889b8429b71e25
OmkarSawantt <162661858+OmkarSawantt@users.noreply.github.com>
2024-06-13 10:13:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors({credentials:true, origin:['https://blog-bytes.vercel.app', 'http:/
 app.use(upload())
 app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
 app.get('/', (req, res) => {
-  res.send('<h1>Hello, World!</h1><img src="/uploads/your_image.jpg" alt="My Image">');
+  res.send('<h1>Hello, World!</h1><img src="/uploads/images.jpg" alt="My Image">');
 });
 app.use('/api/users',userRoutes)
 app.use('/api/posts',postRoutes)
