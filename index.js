68d059f43a5c19b2636db194dec1df7b6cafa216
OmkarSawantt <162661858+OmkarSawantt@users.noreply.github.com>
2024-06-15 10:43:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,25 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const path = require('path');
-require('dotenv').config();
-const upload = require('express-fileupload');
+const express =require('express')
+const cors=require('cors')
+const {connect, default: mongoose} =require ('mongoose')
 
-const userRoutes = require('./routes/userRoutes');
-const postRoutes = require('./routes/postsRoutes');
-const { notFound, errorHandler } = require('./middleware/errorMiddleware');
+require('dotenv').config()
+const upload=require('express-fileupload')
 
-const app = express();
+const userRoutes=require('./routes/userRoutes')
+const postRoutes=require('./routes/postsRoutes')
+const {notFound ,errorHandler}=require('./middelware/errorMiddelware')
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors({ credentials: true, origin: ['https://blog-bytes.vercel.app', 'http://localhost:3000'] }));
-app.use(upload());
-app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
+const app=express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+app.use(express.json({extended:true}))
+app.use(express.urlencoded({extended:true}))
+app.use(cors({credentials:true, origin:['https://blog-bytes.vercel.app', 'http://localhost:3000']}))
+app.use(upload())
+app.use('/uploads',express.static(__dirname+'/uploads'))
 
-app.use('/api/users', userRoutes);
-app.use('/api/posts', postRoutes);
-app.use(notFound);
-app.use(errorHandler);
-
-const url = process.env.MONGO__URI;
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(process.env.PORT || 5000, () => console.log(`Server running on port ${process.env.PORT || 5000}`)))
-  .catch(error => console.error('MongoDB connection error:', error));
+app.use('/api/users',userRoutes)
+app.use('/api/posts',postRoutes)
+app.use(notFound)
+app.use(errorHandler)
+const url=process.env.MONGO__URI
+mongoose.connect(url).then(app.listen(5000,()=>console.log(`server running on port ${process.env.PORT}`))).catch(error=>(console.log(error)))
